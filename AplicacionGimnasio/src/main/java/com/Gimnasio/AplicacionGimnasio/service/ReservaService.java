60830aff7961abaa8dfcf24235ddf3995e622fed
Claudiu Florin Oneata <73060826+claudio1697@users.noreply.github.com>
2022-02-03 10:32:07 +0100

    Update ReservaService.java
    He añadido el metodo de borrar Reserva aqui

--- AplicacionGimnasio/src/main/java/com/Gimnasio/AplicacionGimnasio/service/ReservaService.java
+++ AplicacionGimnasio/src/main/java/com/Gimnasio/AplicacionGimnasio/service/ReservaService.java
@@ -10,4 +10,6 @@ public interface ReservaService {
     Set<Reserva> findAll();
     Reserva anyadirReservaBD(Reserva reserva);
     Reserva modificationeReserva(long id, Reserva reserva);
+    //Cambios que he realizado @Claudio
+    void deleteReserva(long id);
 }
